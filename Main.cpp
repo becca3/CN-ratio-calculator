b038dc3f542d2444098a28964c2e1402ba7488d8
becca3 <32517979+becca3@users.noreply.github.com>
2020-05-10 17:18:53 +0100

    Part of calculator

--- Main.cpp
+++ Main.cpp
@@ -6,7 +6,36 @@ ratio of C (carbon) to N (nitrogen)
 #include <iostream>
 #include <cmath>
 
+int c;
+int n;
+int AM;
+
+void calculate()
+{
+	int percentc;
+	int percentn;
+	int AMN = 14;
+	int AMC = 12;
+
+	percentc = c / AMC;
+	percentn = n / AMN;
+
+	float total;
+	total = percentc / percentn;
+
+	std::cout << total << "\n";
+}
+
 int main(int argc, char** argv)
 {
+	std::cout << "Enter percentage of carbon (C): ";
+	std::cin >> c;
+	std::cout << "Enter percentage of nitrogen (N): ";
+	std::cin >> n;
+
+	calculate();
+	std::cout << "Complete!";
+
+
 	return 0;
 }
