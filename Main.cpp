f13774f723e4da1bed6f7ede6b62f8e7082d5ec1
becca3 <32517979+becca3@users.noreply.github.com>
2020-05-14 19:24:20 +0100

    Need to fix formatting issues

--- Main.cpp
+++ Main.cpp
@@ -6,38 +6,64 @@ Formula:
 %C/%N * 1.166181001
 */
 
+//#include <iostream>
+//#include <cmath>
+//
+////Percentge of carbon.
+//float percentc;
+////Percentage of nitrogen.
+//float percentn;
+//
+//void calculate()
+//{
+//	//Total of %C / %N.
+//	float total;
+//	//Total * 1.166181001.
+//	float final;
+//
+//	total = (percentc / percentn);
+//
+//	final = total * 1.166181001;
+//
+//	std::cout << final << ":1" << "\n";
+//}
+//
+//int main(int argc, char** argv)
+//{
+//	std::cout << "Enter percentage of carbon (C): ";
+//	std::cin >> percentc;
+//	std::cout << "Enter percentage of nitrogen (N): ";
+//	std::cin >> percentn;
+//
+//	calculate();
+//	std::cout << "Complete!";
+//
+//
+//	return 0;
+//}
+
 #include <iostream>
-#include <cmath>
+#include <fstream>
+#include <string>
 
-//Percentge of carbon.
-float percentc;
-//Percentage of nitrogen.
-float percentn;
-
-void calculate()
-{
-	//Total of %C / %N.
-	float total;
-	//Total * 1.166181001.
-	float final;
-
-	total = (percentc / percentn);
-
-	final = total * 1.166181001;
-
-	std::cout << final << ":1" << "\n";
-}
+using namespace std;
 
 int main(int argc, char** argv)
 {
-	std::cout << "Enter percentage of carbon (C): ";
-	std::cin >> percentc;
-	std::cout << "Enter percentage of nitrogen (N): ";
-	std::cin >> percentn;
+	std::ifstream file;
+	file.open("percent.csv");
 
-	calculate();
-	std::cout << "Complete!";
+	if (!file.is_open());
+	std::cout << "error, file is already open. \n";
 
+	while (file.good())
+	{
+		string value;
+		file >> value;
+		file.ignore(SIZE_MAX, '\n');
 
+		std::cout << value << "," << "\n";
+	}
 	return 0;
 }
+
